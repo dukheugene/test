b9a89948c1d549ab594748d74b77d46397a0966e
Yauheni Dukhounik <Yauheni_Dukhounik@epam.com>
2019-04-25 12:31:56 +0300

    Add project files.

---
+++ ProjectForGitCommit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectForGitCommit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
